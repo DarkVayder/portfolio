20852f2ac665aecb1bba3c673a0de38a80670522
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-08-28 11:17:42 +0100

    feat: updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ const textVariants = {
 const Projects = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-      <h1 className="my-20 text-center text-4xl">Projects</h1>
+      <h1 className="my-20 text-center text-4xl">Some of my Projects</h1>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className='mb-8 flex flex-wrap lg:justify-center'> 
